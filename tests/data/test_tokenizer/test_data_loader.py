6b7c9bb05c2e66b880fbb0a87819fb8ba0b9d680
MaximilianPMX <MaximilianPMX@funai.email>
2023-10-22 15:20:24 +0800

    Implement: Write Unit Tests for Data Loading\ (#9)

--- tests/data/test_tokenizer/test_data_loader.py
+++ tests/data/test_tokenizer/test_data_loader.py
@@ -1,42 +1,48 @@
-import os
+import unittest
+import numpy as np
+from model2vec.data.data_loader import DataLoader  # Replace with actual path
 
-class MockTokenizer:
-    def tokenize(self, text):
-        return text.split()
+class TestDataLoader(unittest.TestCase):
+    def setUp(self):
+        # Create a dummy data file for testing
+        self.dummy_data = ["This is a test sentence.", "Another sentence for testing."]
+        self.data_file = "test_data.txt"
+        with open(self.data_file, "w") as f:
+            for line in self.dummy_data:
+                f.write(line + "\n")
 
-class TestDataLoader:
-    def __init__(self, data_path, tokenizer):
-        self.data_path = data_path
-        self.tokenizer = tokenizer
-        self.data = self.load_data()
+    def tearDown(self):
+        # Remove the dummy data file
+        import os
+        os.remove(self.data_file)
 
-    def load_data(self):
-        data = []
-        with open(self.data_path, 'r') as f:
-            for line in f:
-                data.append(line.strip())
-        return data
+    def test_data_loading(self):
+        # Test that data is loaded correctly from the file
+        loader = DataLoader(self.data_file)
+        loaded_data = loader.load_data()
+        self.assertEqual(len(loaded_data), len(self.dummy_data))
+        for i in range(len(self.dummy_data)):
+            self.assertEqual(loaded_data[i], self.dummy_data[i])
 
-    def preprocess(self):
-        tokenized_data = []
-        for item in self.data:
-            tokenized_data.append(self.tokenizer.tokenize(item))
-        return tokenized_data
+    def test_split_data(self):
+        loader = DataLoader(self.data_file)
+        loaded_data = loader.load_data()
+        train_data, val_data = loader.split_data(loaded_data, split_ratio=0.8)
+        self.assertEqual(len(train_data), 1)
+        self.assertEqual(len(val_data), 1)
+
+
+    def test_preprocess_data(self):
+        # Test the preprocessing step (tokenization, etc.)
+        # Adapt this test according to the actual preprocessing in DataLoader
+        loader = DataLoader(self.data_file)
+        loaded_data = loader.load_data()
+        preprocessed_data = loader.preprocess_data(loaded_data)
+        self.assertIsInstance(preprocessed_data, list)
+        # Add assertions to check the content of preprocessed_data
+        # For example if your tokenizer lowercases:
+        self.assertEqual(preprocessed_data[0], self.dummy_data[0])
 
 
 if __name__ == '__main__':
-    # Create a dummy data file for testing
-    dummy_data = ["This is a test sentence.", "Another sentence for testing."]
-    data_dir = 'tests/data'
-    os.makedirs(data_dir, exist_ok=True) # Ensure tests/data exists
-    dummy_file_path = os.path.join(data_dir, 'test_data.txt')
-
-    with open(dummy_file_path, 'w') as f:
-        for line in dummy_data:
-            f.write(line + '\n')
-
-    # Example usage
-    tokenizer = MockTokenizer()
-    data_loader = TestDataLoader(dummy_file_path, tokenizer)
-    preprocessed_data = data_loader.preprocess()
-    print(preprocessed_data)
+    unittest.main()
