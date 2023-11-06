f0e59b568c0bf1890e8bff8749e9446fcee05579
MaximilianPMX <MaximilianPMX@funai.email>
2023-11-06 11:08:03 +0800

    Implement: Add Logging and Checkpointing (Training)\ (#5)

--- model2vec/model2vec/training/trainer.py
+++ model2vec/model2vec/training/trainer.py
@@ -1,37 +1,57 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import logging
+import os
 
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class Trainer:
-    def __init__(self, model, data_loader, optimizer, loss_fn, device='cpu'):
+    def __init__(self, model, optimizer, criterion, config):
         self.model = model
-        self.data_loader = data_loader
         self.optimizer = optimizer
-        self.loss_fn = loss_fn
-        self.device = device
+        self.criterion = criterion
+        self.config = config
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.model.to(self.device)
 
-    def train(self, epochs):
+        # Create checkpoint directory if it doesn't exist
+        if not os.path.exists(self.config.checkpoint_dir):
+            os.makedirs(self.config.checkpoint_dir)
+
+    def train(self, data_loader):
         self.model.train()
-        for epoch in range(epochs):
+        for epoch in range(self.config.num_epochs):
             running_loss = 0.0
-            for i, data in enumerate(self.data_loader):
-                inputs, labels = data
-                inputs = inputs.to(self.device)
-                labels = labels.to(self.device)
+            for i, data in enumerate(data_loader):
+                inputs = data['input'].to(self.device)
+                labels = data['label'].to(self.device)
 
                 self.optimizer.zero_grad()
 
                 outputs = self.model(inputs)
-                loss = self.loss_fn(outputs, labels)
+                loss = self.criterion(outputs, labels)
                 loss.backward()
                 self.optimizer.step()
 
                 running_loss += loss.item()
 
-                if i % 100 == 99:  # Print every 100 mini-batches
-                    print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 100:.3f}')
+                # Log training information
+                if (i + 1) % self.config.log_interval == 0:
+                    avg_loss = running_loss / self.config.log_interval
+                    logging.info(f'Epoch [{epoch+1}/{self.config.num_epochs}], Step [{i+1}/{len(data_loader)}], Loss: {avg_loss:.4f}')
                     running_loss = 0.0
 
-        print('Finished Training')
+            # Checkpoint the model
+            if (epoch + 1) % self.config.checkpoint_frequency == 0:
+                checkpoint_path = os.path.join(self.config.checkpoint_dir, f'model_epoch_{epoch+1}.pth')
+                torch.save({
+                    'epoch': epoch + 1,
+                    'model_state_dict': self.model.state_dict(),
+                    'optimizer_state_dict': self.optimizer.state_dict(),
+                    'loss': loss,
+                }, checkpoint_path)
+                logging.info(f'Checkpoint saved to {checkpoint_path}')
+
+        logging.info('Finished Training')
--- model2vec/model2vec/utils/config.py
+++ model2vec/model2vec/utils/config.py
@@ -1,28 +1,10 @@
-import json
-
 class Config:
-    def __init__(self, config_path='config.json'):
-        self.config_path = config_path
-        try:
-            with open(self.config_path, 'r') as f:
-                self.config = json.load(f)
-        except FileNotFoundError:
-            self.config = {}
-            print(f"Config file not found at {self.config_path}. Using default configuration.")
-
-    def get(self, key, default=None):
-        return self.config.get(key, default)
-
-    def set(self, key, value):
-        self.config[key] = value
-        self.save()
-
-    def save(self):
-        with open(self.config_path, 'w') as f:
-            json.dump(self.config, f, indent=4)
-
-if __name__ == '__main__':
-    config = Config()
-    print(f'Current learning rate: {config.get("learning_rate", 0.001)}')
-    config.set("learning_rate", 0.002)
-    print(f'Updated learning rate: {config.get("learning_rate", 0.001)}')
+    def __init__(self):
+        self.embedding_dim = 128
+        self.hidden_dim = 256
+        self.num_epochs = 10
+        self.batch_size = 32
+        self.learning_rate = 0.001
+        self.log_interval = 10  # Log training loss every 10 batches
+        self.checkpoint_frequency = 2  # Save checkpoint every 2 epochs
+        self.checkpoint_dir = 'checkpoints'  # Directory to save checkpoints
